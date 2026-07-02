c683a1adbe2d77dd8736c67932eee3d080badaf5
Ilija <ilijastevanovic005@gmail.com>
2026-07-02 21:33:01 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center ">
+        <h1 className="font-display text-6xl uppercase leading-none text-white md:text-8xl">
+          FILP AND FIND
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
