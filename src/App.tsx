c5aa7960bfd2004d3594e240132b3491227f2202
Janko <jankomilosavljeviccc@gmail.com>
2026-07-03 17:40:37 +0200

    React-Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Home />} />)
+);
+
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
+
 export default App;
